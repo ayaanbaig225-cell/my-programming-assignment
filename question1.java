28f7c957e7418889fba2c07bbc9b41ab2b393cdd
ayaanbaig225-cell <ayaanbaig225@gmail.com>
2026-01-09 19:56:20 +0500

    Add files via upload

---
+++ question1.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+public class question1 {
+    public static void main(String[] args) {
+
+ 
+Scanner scan = new Scanner(System.in);
+
+System.out.println("enter obtained marks");
+float a1 = scan.nextInt();
+System.out.println("enter maximum marks");
+float b1 = scan.nextInt();
+float p = (a1/b1*100);
+System.out.println(p);
+
+
+    }
+}
